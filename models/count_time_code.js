1b82c31f110b2ffdae84df95efd7402207b40d84
maitrongnhan001 <maitrongnhan.001@gmail.com>
2021-12-21 21:01:48 +0700

    init models

---
+++ models/count_time_code.js
@@ -0,0 +1,37 @@
+const { connection } = require('../config/database.js');
+
+module.exports.getTotalTimeCode = (id) => {
+    return new Promise((reslove, reject) => {
+
+    });
+}
+
+module.exports.getTotalTimeByDays = (id) => {
+    return new Promise((reslove, reject) => {
+
+    });
+}
+
+module.exports.getTotalTimeByWeeks = (id) => {
+    return new Promise((reslove, reject) => {
+
+    });
+}
+
+module.exports.getTotalTimeByMonths = (id) => {
+    return new Promise((reslove, reject) => {
+
+    });
+}
+
+module.exports.startCode = (time, id) => {
+    return new Promise((resolve, reject) => {
+        //function set time start code
+    });
+}
+
+module.exports.pauseCode = (time, id) => {
+    return new Promise((resolve, reject) => {
+        //function set time start code
+    });
+}
---
+++ models/user.js
@@ -0,0 +1,19 @@
+const { connection } = require('../config/database.js');
+
+module.exports.getUser = (id) => {
+    return new Promise ((resolve, reject) => {
+        //get user
+    });
+}
+
+module.exports.createUser = (user) => {
+    return new Promise ((resolve, reject) => {
+        //create user
+    });
+}
+
+module.exports.updateUser = (user) => {
+    return new Promise ((resolve, reject) => {
+        //update user
+    });
+}
