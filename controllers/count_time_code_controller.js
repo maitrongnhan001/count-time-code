bb219e9d6645eec498f50140d100834aa9165e35
maitrongnhan001 <maitrongnhan.001@gmail.com>
2021-12-21 21:01:22 +0700

    init controllers

---
+++ controllers/count_time_code_controller.js
@@ -0,0 +1,25 @@
+const count_time_code = require('../models/count_time_code.js');
+
+module.exports.getTotalTimeCode = async (req, res) => {
+    //get total time code
+}
+
+module.exports.getDashBoardByDays = async (req, res) => {
+    //get dashboard time code by days
+}
+
+module.exports.getDashBoardByWeeks = async (req, res) => {
+    //get dashboard time code by weeks
+}
+
+module.exports.getDashBoardByMonths = async (req, res) => {
+    //get dashboard time code by months
+}
+
+module.exports.startCode = async (req, res) => {
+    //start code
+}
+
+module.exports.pauseCode = async (req, res) => {
+    //pause code
+}
---
+++ controllers/user_controller.js
@@ -0,0 +1,17 @@
+const user = require('../models/user.js');
+
+module.exports.createUserController = async (req, res) => {
+    //create user
+}
+
+module.exports.updateUserController = async (req, res) => {
+    //update user
+}
+
+module.exports.getUserController = async (req, res) => {
+    //get information of user
+}
+
+module.exports.getRankUsers = async (req, res) => {
+    //get list rank of users
+}
